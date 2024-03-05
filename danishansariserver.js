5c40085c0aa86dc6363f0176bcb82f445f2b3b8e
Danish Ansari <117536349+danishansari04@users.noreply.github.com>
2024-03-05 13:57:15 +0530

    Update danishansariserver.js

--- danishansariserver.js
+++ danishansariserver.js
@@ -24,3 +24,30 @@ function authentication(username,password){
         return false
     }
 }
+
+
+let rmCheck = document.getElementById("rememberMe"),
+    passwordInput = document.getElementById("password"),
+    emailInput = document.getElementById("email");
+
+if (localStorage.checkbox && localStorage.checkbox != "") {
+  rmCheck.setAttribute("checked", "checked");
+  emailInput.value = localStorage.username;
+  passwordInput.value = localStorage.password;
+} else {
+  rmCheck.removeAttribute("checked");
+  emailInput.value = "";
+  passwordInput.value = "";
+}
+
+function lsRememberMe() {
+  if (rmCheck.checked && emailInput.value != "" && passwordInput.value != "") {
+    localStorage.username = emailInput.value;
+    localStorage.password = passwordInput.value;
+    localStorage.checkbox = rmCheck.value;
+  } else {
+    localStorage.username = "";
+    localStorage.passoword = "";
+    localStorage.checkbox = "";
+  }
+}
