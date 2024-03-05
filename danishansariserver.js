6278d7e07db21efddfa57b2b6890b60e52dc1954
Danish Ansari <117536349+danishansari04@users.noreply.github.com>
2024-03-05 14:01:34 +0530

    Update danishansariserver.js

--- danishansariserver.js
+++ danishansariserver.js
@@ -26,28 +26,3 @@ function authentication(username,password){
 }
 
 
-let rmCheck = document.getElementById("rememberMe"),
-    passwordInput = document.getElementById("password"),
-    emailInput = document.getElementById("email");
-
-if (localStorage.checkbox && localStorage.checkbox != "") {
-  rmCheck.setAttribute("checked", "checked");
-  emailInput.value = localStorage.username;
-  passwordInput.value = localStorage.password;
-} else {
-  rmCheck.removeAttribute("checked");
-  emailInput.value = "";
-  passwordInput.value = "";
-}
-
-function lsRememberMe() {
-  if (rmCheck.checked && emailInput.value != "" && passwordInput.value != "") {
-    localStorage.username = emailInput.value;
-    localStorage.password = passwordInput.value;
-    localStorage.checkbox = rmCheck.value;
-  } else {
-    localStorage.username = "";
-    localStorage.passoword = "";
-    localStorage.checkbox = "";
-  }
-}
